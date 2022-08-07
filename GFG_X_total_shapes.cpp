1037c44239e187506d16dad13513c527617c8c5d
ANKIT GOSWAMI <93897456+SHIVAAYgod@users.noreply.github.com>
2022-08-07 08:41:38 +0530

    Update GFG_X_total_shapes.cpp

--- GFG_X_total_shapes.cpp
+++ GFG_X_total_shapes.cpp
@@ -1,3 +1,9 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
 #define pii pair<int,int>
 class Solution
 {
@@ -38,3 +44,24 @@ class Solution
         return shape;
     }
 };
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>grid(n, vector<char>(m, '#'));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.xShape(grid);
+		cout << ans <<'\n';
+	}
+	return 0;
+}
+// } Driver Code Ends

beafe37b2aca7f2f06dcfeb323d7a2a329c1355b
ANKIT GOSWAMI <93897456+SHIVAAYgod@users.noreply.github.com>
2022-08-15 10:18:15 +0530

    Update GFG_friendsPairing.cpp

--- GFG_friendsPairing.cpp
+++ GFG_friendsPairing.cpp
@@ -1,1 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
 
+// } Driver Code Ends
+class Solution
+{
+public:
+long long int dp[10001];
+    long long int solve(int n){
+        if(n == 0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        long long int a = solve(n-1);
+        long long int b = (n-1)*solve(n-2);
+        return dp[n] = (a+b)%1000000007;
+    }
+    
+    int countFriendsPairings(int n) 
+    { 
+        // code here
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};    
+ 
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout <<ob.countFriendsPairings(n); 
+    	cout<<endl;
+    }
+} 
+
+// } Driver Code Ends

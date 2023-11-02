37e71549315f24ded812c1898492fc8fb0744625
ANKIT GOSWAMI <93897456+SHIVAAYgod@users.noreply.github.com>
2023-11-02 11:28:37 +0530

    Update LC_Count Nodes Equal to Average of Subtree.py

--- LC_Count Nodes Equal to Average of Subtree.py
+++ LC_Count Nodes Equal to Average of Subtree.py
@@ -1,23 +1,29 @@
 class Solution:
-    def averageOfSubtree(self, root: TreeNode) -> int:
-        result = 0
+    def __init__(self):
+        self.matchingSubtreeCount = 0  # Initialize the count of subtrees with matching averages.
 
-        def traverse(node):
-            nonlocal result
-            
-            if not node:
-                return 0, 0
-            
-            left_sum, left_count = traverse(node.left)
-            right_sum, right_count = traverse(node.right)
-            
-            curr_sum = node.val + left_sum + right_sum
-            curr_count = 1 + lefta_count + right_count
-            
-            if curr_sum // curr_count == node.val:
-                result += 1
-            
-            return curr_sum, curr_count
-        
-        traverse(root)
-        return result
+    # A Depth-First Search (DFS) function that returns a tuple of two values:
+    # - The sum of values within the current subtree.
+    # - The number of nodes within the current subtree.
+    def calculateSubtreeValues(self, currentNode):
+        if currentNode is None:
+            return 0, 0  # Base case: Return 0 for both sum and number of nodes if the node is None.
+
+        # Recursively calculate values for the left and right subtrees.
+        leftSubtree  = self.calculateSubtreeValues(currentNode.left)
+        rightSubtree = self.calculateSubtreeValues(currentNode.right)
+
+        # Calculate the sum of values and the number of nodes in the current subtree.
+        sumOfValues  = leftSubtree [0] + rightSubtree[0] + currentNode.val
+        numberOfNodes  = leftSubtree [1] + rightSubtree[1] + 1
+
+        # Check if the current node's value matches the average of its subtree.
+        if numberOfNodes  != 0 and sumOfValues  // numberOfNodes  == currentNode.val:
+            self.matchingSubtreeCount += 1
+
+        return sumOfValues , numberOfNodes   # Return the calculated values for the current subtree.
+
+
+    def averageOfSubtree(self, root):
+        self.calculateSubtreeValues(root)  # Start the DFS from the root node.
+        return self.matchingSubtreeCount
